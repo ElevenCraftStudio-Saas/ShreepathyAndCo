b1c92450d712434e77db31248a81c6d80b2d6a5c
jaisudharshan18 <jaisudharshanjaisudharshan.com>
2026-06-16 11:13:49 +0530

    fix: update base path to ShreepathyAndCo and force node24 in workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/shreepathy/',
+  base: '/ShreepathyAndCo/',
   plugins: [react()],
   resolve: {
     alias: {
